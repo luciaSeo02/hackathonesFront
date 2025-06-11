9cd91b82ba83b507db9de049ccdc259983a33fd0
Laia Clemente <laiacp2002@gmail.com>
2025-06-11 16:04:18 +0200

    add context

---
+++ my-react-app/src/services/getDataUserLoggedService.js
@@ -0,0 +1,17 @@
+const getDataUserLoggedService = async ({ token }) => {
+  const url = `${import.meta.env.VITE_URL_API}/users/login`;
+
+  const response = await fetch(url, {
+    headers: {
+        authorization: token
+    }
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) throw new Error(json.message);
+
+  return json.data;
+}
+
+export default getDataUserLoggedService
