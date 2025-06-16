a12f53f1aea206e2162ec6b3c183330128d22c68
Laia Clemente <laiacp2002@gmail.com>
2025-06-16 18:18:44 +0200

    add 'logo'

--- my-react-app/src/components/HeaderComponent.jsx
+++ my-react-app/src/components/HeaderComponent.jsx
@@ -5,7 +5,7 @@ const HeaderComponent = () => {
     return (
         <header className="bg-[#afafaf70] backdrop-blur-sm sticky top-2.5 mx-10 p-2 rounded-lg flex justify-between items-center sm:top-10 sm:right-10 sm:left-10 sm:p-4 sm:rounded-[10px]">
             <Link to={'/'}>
-                <h3>LOGO</h3>
+                <h3>HackNMeet</h3>
             </Link>
 
             <NavBar />
