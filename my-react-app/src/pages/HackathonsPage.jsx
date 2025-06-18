f7dfe27977a14758ba4b502e08b8df0fe395c8c2
Lucia Seoane <luciaseo20@gmail.com>
2025-06-18 07:54:04 +0200

    añadir boton para crear hackathon

--- my-react-app/src/pages/HackathonsPage.jsx
+++ my-react-app/src/pages/HackathonsPage.jsx
@@ -1,9 +1,31 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContextProvider';
+
 import HackathonsList from '../components/HackathonsList';
+import Button from '../components/ui/Button';
 
 const HackathonsPage = () => {
+    const { userLogged } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleCreateClick = () => {
+        navigate('/hackathons/create');
+    };
+
     return (
         <div className="p-4 md:p-8">
-            <h2 className="text-2xl font-bold mb-6">Lista de Hackathones</h2>
+            <div className="flex justify-between items-center mb-6">
+                <h2 className="text-2xl font-bold">Lista de Hackathones</h2>
+
+                {userLogged?.role === 'admin' && (
+                    <Button
+                        text="Crear hackathon"
+                        onClick={handleCreateClick}
+                    />
+                )}
+            </div>
+
             <HackathonsList />
         </div>
     );
