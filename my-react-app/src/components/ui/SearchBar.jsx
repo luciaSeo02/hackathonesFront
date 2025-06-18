d938330f2da8b807ced8f0b7ec26d091d96ff1bc
Diego Pazos <diegopazos9@gmail.com>
2025-06-18 13:49:02 +0200

    Cambios NavBar, About y contact pages

--- my-react-app/src/components/ui/SearchBar.jsx
+++ my-react-app/src/components/ui/SearchBar.jsx
@@ -4,7 +4,13 @@ const SearchBar = () => {
     return (
         <div className="bg-neutral-100 w-60 px-[10px] py-2 rounded-lg flex items-center gap-[10px]">
             <div>
-                <Search size={15} />
+                <Search size={15} 
+                width="15" 
+                height="15" 
+                fill="none" 
+                stroke="#5F3DC4" 
+                strokeWidth="2" 
+                viewBox="0 0 24 24" />
             </div>
 
             <p className="text-[#afafaf] text-xs md:text-sm">
@@ -15,3 +21,5 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+
+
