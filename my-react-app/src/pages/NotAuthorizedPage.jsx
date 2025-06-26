197234e16e8f075df438e1f7a881492c5fe434bc
Lucia Seoane <luciaseo20@gmail.com>
2025-06-26 21:21:27 +0200

    corregir responsive

--- my-react-app/src/pages/NotAuthorizedPage.jsx
+++ my-react-app/src/pages/NotAuthorizedPage.jsx
@@ -1,19 +1,19 @@
 const NotAuthorizedPage = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="relative w-24 h-32 mb-14">
+        <div className="min-h-screen flex flex-col items-center justify-center p-5">
+            <div className="relative w-24 h-32 md:mb-14 mb-5">
                 <img
                     src="/notAuthorizedLogo.PNG"
                     alt="Acceso denegado"
                     className="w-full h-full object-cover z-0 relative top-3 opacity-90"
                 />
 
-                <h2 className="absolute -bot-0 left-1/2 -translate-x-1/2 text-5xl font-bold text-indigo-900 z-10 whitespace-nowrap">
+                <h2 className="absolute left-1/2 -translate-x-1/2 -md:text-5xl font-bold text-indigo-900 z-10 whitespace-nowrap">
                     Acceso denegado
                 </h2>
             </div>
 
-            <p className="text-lg text-gray-700 text-center">
+            <p className="text-lg text-gray-700 text-center text-base md:text2xl mt-5">
                 Lo sentimos, no tienes permiso para ver esta página.
             </p>
         </div>
